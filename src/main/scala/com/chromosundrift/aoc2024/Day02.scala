870452c5c71310421481aeeb5d1d4c1d8401b1d6
Chris Mountford <chris.mountford@gmail.com>
2024-12-03 21:05:16 +1100

    omg what a comedy of errors

---
+++ src/main/scala/com/chromosundrift/aoc2024/Day02.scala
@@ -0,0 +1,92 @@
+package com.chromosundrift.aoc2024
+
+import scala.io.Source
+
+
+def getTestInput1 = {
+  Source.fromResource("day02_input1.txt").getLines().toList
+}
+
+object Day02 {
+
+
+  def dump[A](x: List[A]): Unit = {
+    println(x.mkString(", "))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val reports = getTestInput1
+    println(part1(reports))
+    println(part2(reports))
+  }
+
+  def deltas(levels: List[Int]): List[Int] = {
+    levels.sliding(2).map(pairs => pairs(1) - pairs(0)).toList
+  }
+
+  def ints(line: String): List[Int] = {
+    line.split("\\s+").map(x => x.toInt).toList
+  }
+
+  def inRange(delta: Int): Boolean = {
+    val abs = Math.abs(delta)
+    abs >= 1 && abs <= 3
+  }
+
+  def monotonic(ds: List[Int]): Boolean = {
+    consistentSigns(ds).forall(identity)
+  }
+
+  private def consistentSigns(ds: List[Int]): List[Boolean] = {
+    ds.sliding(2).map(ds => ds(0).sign == ds(1).sign).toList
+  }
+
+  def part1(lines: List[String]): Int = {
+    val reports = lines.map(ints)
+    val ds = reports.map(x => deltas(x))
+    val isSafes = ds.map { (x: List[Int]) =>
+      val isMonotonic = monotonic(x)
+      val allInRange = x.map(inRange).forall(identity)
+      isMonotonic && allInRange
+    }
+    isSafes.count(identity)
+  }
+
+  def part1Alt(lines: List[String]): Int = {
+    val reports = lines.map(ints)
+    val safeList = reports.map(allSafe)
+    // return the number of safe reports
+    safeList.count(identity)
+  }
+
+  def allSafe(report: List[Int]): Boolean = {
+    val ds = deltas(report).toList
+    val monotonics: Seq[Boolean] = consistentSigns(ds).toList
+    val inRanges: Seq[Boolean] = ds.map(inRange).toList
+    return monotonics.forall(identity) && inRanges.forall(identity)
+  }
+
+  private def part2SafeCount(report: List[Int]): Boolean = {
+    allSafe(report) || tryRemoving1(report).isDefined
+  }
+
+  def part2(lines: List[String]): Int = {
+    val is: Seq[List[Int]] = lines.map(a => ints(a))
+    val safes: Seq[Boolean] = is.map(report => part2SafeCount(report))
+    safes.count(identity)
+  }
+
+  def tryRemoving1(report: List[Int]): Option[Int] = {
+    for (i <- report.indices) {
+      // remove one
+      val oneRemoved = report.zipWithIndex.filter(_._2 != i)
+      val newSafeness = allSafe(oneRemoved.map(_._1))
+      if (newSafeness) {
+        return Some(i)
+      }
+    }
+    return None
+  }
+}
+
+
---
+++ src/test/scala/com/chromosundrift/aoc2024/Day02Spec.scala
@@ -0,0 +1,113 @@
+package com.chromosundrift.aoc2024
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day02Spec extends AnyFlatSpec with Matchers {
+
+  val testInput: List[String] = List(
+    "7 6 4 2 1",  // safe
+    "1 2 7 8 9",  // jump up too big
+    "9 7 6 2 1",  // jump down too big
+    "1 3 2 4 5",  // change of direction - nonmonotonic
+    "8 6 4 4 1",  // no change disallowed
+    "1 3 6 7 9",  // safe
+  )
+
+
+  "day02 part 1" should "give whole correct test output" in {
+    Day02.part1(testInput) shouldBe 2
+  }
+
+  it should "give whole correct test output for alt" in {
+    Day02.part1Alt(testInput) shouldBe 2
+  }
+
+  it should "work with one record" in {
+    val safeRecord = testInput.take(1)
+    safeRecord shouldBe List("7 6 4 2 1")
+    Day02.part1Alt(safeRecord) shouldBe 1
+  }
+
+  it should "work with nonmonotonic record" in {
+    Day02.part1Alt(List(testInput(3))) shouldBe 0
+  }
+
+  "safes" should "work with test input" in {
+    val records: Seq[List[Int]] = testInput.map(Day02.ints)
+    val safes: Seq[Boolean] = records.map(Day02.allSafe)
+    safes shouldBe Seq(true, false, false, false, false, true)
+  }
+
+  it should "show first test input as safe" in {
+    val ints = Day02.ints(testInput(0))
+    Day02.allSafe(ints) shouldBe true
+  }
+
+
+  "internal functions" should "detect monotonic sequences" in {
+    val deltas = List(1,4,5,1,3,5)
+    Day02.monotonic(deltas) shouldBe true
+  }
+
+  it should "handle all negative deltas as monotonic sequences" in {
+    val deltas = List(-1,-4,-5,-1,-3,-5)
+    Day02.monotonic(deltas) shouldBe true
+  }
+
+  it should "not show nonmonotonic as monotonic" in {
+    val deltas = List(1, 4, -5, 1, 3, 5)
+    Day02.monotonic(deltas) shouldBe false
+  }
+
+  it should "treat zero deltas as fail" in {
+    val deltas = List(1, 4, 0, 1, 3, 5)
+    Day02.monotonic(deltas) shouldBe false
+  }
+
+  it should "calculate deltas correctly" in {
+    val numbers = List(1, 2, 7, 8, 9)
+    val deltas = Day02.deltas(numbers).toList
+    deltas shouldBe List(1, 5, 1, 1)
+  }
+  it should "calculate zero deltas correctly" in {
+    val numbers = List(8, 6, 4, 4, 1)
+    val deltas = Day02.deltas(numbers).toList
+    deltas shouldBe List(-2, -2, 0, -3)
+  }
+
+  it should "pass test input for range" in {
+    val records: Seq[List[Int]] = testInput.map(Day02.ints)
+    val deltas = records.map(x => (x, Day02.deltas(x.toList).toList))
+
+    val inRanges = deltas.map { (x: List[Int], ds: List[Int]) =>
+      val bs: List[Boolean] = ds.map((d: Int) => Day02.inRange(d)).toList
+      bs.length shouldBe ds.length
+      bs.length shouldBe (x.length - 1)
+      !bs.contains(false)
+    }.toList
+    inRanges shouldBe List(true, false, false, true, false, true)
+  }
+
+  it should "pass test input for monotonicity" in {
+    val ints = testInput.map(Day02.ints)
+    val deltas = ints.map(x => (x, Day02.deltas(x.toList)))
+    val monos = deltas.map { (x, ds) =>
+      val isMonotonic = Day02.monotonic(ds)
+      isMonotonic
+    }.toList
+    monos shouldBe List(true, true, true, false, false, true)
+  }
+
+  "Day02" should "solve part 1" in {
+    Day02.part1(getTestInput1) shouldBe 371
+  }
+
+  it should "solve part 1 Alt" in {
+    Day02.part1Alt(getTestInput1) shouldBe 371
+  }
+
+  it should "pass test input part 2" in {
+    Day02.part2(testInput) shouldBe 4
+  }
+}
