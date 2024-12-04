c91f0b51c42759de76981900e58158a566a83b2b
Chris Mountford <chris.mountford@gmail.com>
2024-12-04 17:58:42 +1100

    day4

---
+++ src/main/scala/com/chromosundrift/aoc2024/Day04.scala
@@ -0,0 +1,108 @@
+package com.chromosundrift.aoc2024
+
+import scala.io.Source
+
+type DeltaSpec = Seq[List[(Int, Int)]]
+
+object Day04 {
+  val part1Sample =
+    """MMMSXXMASM
+      |MSAMXMSMSA
+      |AMXSXMAAMM
+      |MSAMASMSMX
+      |XMASAMXAMM
+      |XXAMMXXAMA
+      |SMSMSASXSS
+      |SAXAMASAAA
+      |MAMMMXMMMM
+      |MXMXAXMASX""".stripMargin
+
+  val part2Sample =
+    """.M.S......
+      |..A..MSMS.
+      |.M.S.MAA..
+      |..A.ASMSM.
+      |.M.S.M....
+      |..........
+      |S.S.S.S.S.
+      |.A.A.A.A..
+      |M.M.M.M.M.
+      |..........""".stripMargin
+
+  val part1SampleOccurrences = 18
+  val part1Targets = List("XMAS", "SAMX")
+  // only need to handle half the directions: LtR, TLtBR, TtB, LRtBL
+  // because we search for both forwards and backwards words
+  val part1Deltas: DeltaSpec = List(
+    List((0, 0), (0, 1), (0, 2), (0, 3)), // left to right
+    List((0, 0), (1, 1), (2, 2), (3, 3)), // top left to bottom right
+    List((0, 0), (1, 0), (2, 0), (3, 0)), // top to bottom
+    List((0, 0), (1, -1), (2, -2), (3, -3)), // top right to bottom left
+  )
+
+  val part2Targets = List("MASMAS", "MASSAM", "SAMMAS", "SAMSAM")
+  val part2Deltas: DeltaSpec = List(
+    List((0, 0), (1, 1), (2, 2), (0, 2), (1, 1), (2, 0)), // TLtBR,BLtTR
+  )
+
+  def getTestInput1 = {
+    Source.fromResource("day04_input1.txt").mkString
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = getTestInput1
+    println(solve(part1Targets, part1Deltas, part1Sample))
+    println(solve(part1Targets, part1Deltas, input))
+    println(solve(part2Targets, part2Deltas, part2Sample))
+    println(solve(part2Targets, part2Deltas, input))
+  }
+
+  def solve(targets: List[String], deltas: DeltaSpec, input: String): Int = {
+    var wordCount = 0
+    val rows = input.split('\n')
+
+    // we assume for now we have rectangle puzzle
+
+    assert(rows.length > 0)
+    val rowLength = rows(0).length
+    assert(rows.forall(r => r.length == rowLength))
+
+    for (j <- rows.indices) {
+      val row = rows(j)
+      for (i <- row.indices) {
+        // both can't succeed but code is simpler this way
+        wordCount += targets.map(t => findWords(t, rows, i, j, deltas)).sum
+      }
+    }
+    return wordCount
+  }
+
+  def findWords(needle: String, haystack: Array[String], i: Int, j: Int, deltas: DeltaSpec): Int = {
+    var wordCount = 0
+    deltas.foreach { strip =>
+      // there is room for the word, check and apply each delta
+      var found = true
+      for (index <- needle.indices) {
+        val letterDelta = strip(index)
+        val jj = j + letterDelta._1
+        val ii = i + letterDelta._2
+        if (jj < 0 || jj >= haystack.length || ii < 0 || ii >= haystack(jj).length) {
+          // out of bounds
+          found = false
+        } else {
+          val findLetter = needle(index)
+          //        val deltaSpec = s"EndDeltaOffset: ${iEndDeltaOffset},${jEndDeltaOffset} lastDelta: ${lastDelta}"
+          //        println(s" ${findLetter} base: $i,$j @offset $ii,$jj | ${strip} | ${deltaSpec}")
+          if (needle(index) != haystack(jj)(ii)) {
+            found = false
+          }
+        }
+      }
+      if (found) {
+        //        println(s"found ${needle} at ${i},${j} using delta ${strip}")
+        wordCount += 1
+      }
+    }
+    return wordCount
+  }
+}
