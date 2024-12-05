a7dd9e933c46a3616df3272decd805c7bf69a86e
Chris Mountford <chris.mountford@gmail.com>
2024-12-05 23:26:10 +1100

    clean up solution for day 1

--- src/main/scala/com/chromosundrift/aoc2024/Day01.scala
+++ src/main/scala/com/chromosundrift/aoc2024/Day01.scala
@@ -2,26 +2,20 @@ package com.chromosundrift.aoc2024
 
 import scala.io.Source
 
-
+//noinspection ScalaWeakerAccess
 object Day01 {
   def main(args: Array[String]): Unit = {
-    val (left, right) = Source.fromResource("day01_input1.txt").getLines().map { line =>
-      val parts = line.split("\\s+")
-      (parts(0).toInt, parts(1).toInt)
-    }.toList.unzip
+    val (left, right) = Source.fromResource("day01_input1.txt").getLines()
+      .map(_.split("\\s+").map(_.toInt))
+      .map { case Array(l, r) => (l, r) }
+      .toList.unzip
     val lSorted = left.sorted
     val rSorted = right.sorted
-    part01(lSorted zip rSorted)
-    part02(lSorted, rSorted)
+    println(part01(lSorted zip rSorted))
+    println(part02(lSorted, rSorted))
   }
 
-  private def part01(sortedPairs: List[(Int, Int)]): Unit = {
-    val sum = sortedPairs.map((p: (Int, Int)) => Math.abs(p._1 - p._2)).sum
-    println(sum)
-  }
+  def part01(sortedPairs: List[(Int, Int)]): Int = sortedPairs.map(p => (p._1 - p._2).abs).sum
 
-  private def part02(left: List[Int], right: List[Int]): Unit = {
-    val sum = left.map(v => v * right.count(i => i == v)).sum
-    println(sum)
-  }
+  def part02(left: List[Int], right: List[Int]): Int = left.map(v => v * right.count(_ == v)).sum
 }
