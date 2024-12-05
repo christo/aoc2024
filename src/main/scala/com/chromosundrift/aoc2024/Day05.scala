f67d1199983c9757f2cd5ddd38084e0f2f0cef41
Chris Mountford <chris.mountford@gmail.com>
2024-12-05 17:22:27 +1100

    day5 works

---
+++ src/main/scala/com/chromosundrift/aoc2024/Day05.scala
@@ -0,0 +1,89 @@
+package com.chromosundrift.aoc2024
+
+import scala.io.Source
+
+type Rules = Array[(Int, Int)]
+type PageSequence = Array[Int]
+case class Day05Input(rules: Rules, pages: Array[PageSequence])
+
+
+object Day05 {
+  private def getInput(file: String): String = {
+    Source.fromResource(file).mkString
+  }
+
+  def parseInput(s: String): Day05Input = {
+    val parts = s.split("\n\n")
+    assert(parts.length == 2)
+    val rules = parts(0).split("\n").map { l =>
+      val ints = l.split('|').map(_.toInt)
+      assert(ints.length == 2)
+      val rule = (ints(0), ints(1))
+      rule
+    }
+    val pages: Array[Array[Int]] = parts(1).split("\n").map(_.split(",").map(_.toInt))
+    new Day05Input(rules, pages)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = parseInput(getInput("day05_input1.txt"))
+    println(part01(input))
+    println(part02(input))
+  }
+
+  def inOrder(ints: PageSequence, rules: Rules): Boolean = {
+    val pages: Array[(Int, Int)] = ints.zipWithIndex
+    rules.forall { r =>
+      val before: Option[(Int, Int)] = pages.find((t: (Int, Int)) => t._1 == r._1)
+      if (before.isDefined) {
+        val after: Option[(Int, Int)] = pages.find((t: (Int, Int)) => t._1 == r._2)
+        if (after.isDefined) {
+          // both before and after pages specified in rule are found
+          // get indices
+          val bi = before.get._2
+          val ai = after.get._2
+          bi < ai
+        } else {
+          // after missing, no rule break
+          true
+        }
+      }
+      else {
+        // before missing, no rule break
+        true
+      }
+    }
+  }
+
+  private def middle(ps: PageSequence): Int = {
+    assert((ps.length % 2) == 1)
+    ps(ps.length/2)
+  }
+
+  def part01(input: Day05Input): Int = {
+    input.pages.filter(inOrder(_, input.rules)).map(middle).sum
+  }
+
+  def mkOrdering(rules: Rules): Ordering[Int] = {
+    new Ordering[Int] {
+      def compare(x: Int, y: Int): Int = {
+        if (x == y) {
+          0
+        } else {
+          val wrong = rules.find((r: (Int, Int)) => x == r._1 && y == r._2)
+          wrong match
+            case Some(_) => -1
+            case _ => 1
+        }
+      }
+    }
+  }
+
+  def part02(input: Day05Input): Int = {
+    input.pages
+      .filter(!inOrder(_, input.rules))
+      .map(ps => ps.sorted(mkOrdering(input.rules)))
+      .map(middle)
+      .sum
+  }
+}
